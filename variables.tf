6842337883b588ec229b1e41cfca513350fb3694
Yasin <ysnhasturk@gmail.com>
2024-01-31 00:06:13 +0300

    first version

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "git-name" {
+  default = "{Write your Github username}"
+}
+
+variable "git-token" {
+  default = "{# Write your TOKEN number}"
+}
+
+variable "key-name" {
+  default = "{Write your pem file name}"
+}
+
+variable "hosted-zone" {
+  default = "{Write your hosted zone name}"
+}
